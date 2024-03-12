da791efd51817fae71d4505eb99e5abf3866952b
KATEKOY <145491056+KATEKOY@users.noreply.github.com>
2024-03-13 01:04:07 +0300

    Main.java

--- src/Zadanie10/Main.java
+++ src/Zadanie10/Main.java
@@ -3,7 +3,7 @@ package Zadanie10;
 import java.util.stream.LongStream;
 public class Main {
     public static void main(String[] args) {
-        System.out.println(sumNumbersBetween(3, 6));
+        System.out.println(sumNumbersBetween(10000000, 1000000000));
     }
     public static long sumNumbersBetween(int a, int b) {
         return LongStream.range(a, b)
