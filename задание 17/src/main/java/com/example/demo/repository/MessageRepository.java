a986c90e88f897e0eb34eb43cba3dbd201327369
KATEKOY <145491056+KATEKOY@users.noreply.github.com>
2024-06-18 21:50:03 +0300

    Delete задание 17 directory

--- задание 17/src/main/java/com/example/demo/repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.dto.Message;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageRepository extends CrudRepository <Message, Integer> {
-}
