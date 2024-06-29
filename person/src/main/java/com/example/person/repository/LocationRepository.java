61127a2c473219e606aafc1ed202a85259d08b00
KATEKOY <145491056+KATEKOY@users.noreply.github.com>
2024-06-30 02:01:41 +0300

    Delete person directory

--- person/src/main/java/com/example/person/repository/LocationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.person.repository;
-import com.example.person.model.Location;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LocationRepository extends CrudRepository<Location, Integer> {
-}
